f35a477326c337109313b7218dcae2c9f116d981
AFarforov <anton@black-sheep.ru>
2012-03-23 17:17:29 +0400

    INITIAL

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "iz-tkani.ru",
-  #:user_name            => "fasexe",
-  #:password             => "fuckoff1985",
-  :user_name            => "muslishire",
-  :password             => "qwerty2642983",
+  :user_name            => "fasexe",
+  :password             => "enlightenment1985",
+  #:user_name            => "muslishire",
+  #:password             => "qwerty2642983",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
